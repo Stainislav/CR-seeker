c8487d0ec944a9e3060b541c164030bd948c434e
Your Name <swatogor@yandex.ru>
2017-08-25 22:47:51 +0300

    imporved get_clinical_research function. Now it can get links from the page.

--- control/functions.py
+++ control/functions.py
@@ -13,9 +13,6 @@ from io import StringIO
 from lxml import etree
 
 from urllib.request import urlopen
-#import urllib.request
-#page = html.parse(urlopen('https://google.com'))
-#from StringIO import StringIO
 
 DATABASE = '/home/stanislav/Dropbox/Programming/CS50/final_project/CR-seeker.git/data/database.db'
 connection = sqlite3.connect(DATABASE)
@@ -23,21 +20,24 @@ db = connection.cursor()
 
 def index():
     mapping = {}
-    #"html/aironepage/HTML/index.html"
+    
     body = render("index", mapping)
     return body
 
 def get_clinical_research():
     page = html.parse(urlopen('https://www.rlsnet.ru/tn_alf_letter_c0.htm'))
-    page = page.getroot()
-    page = page.text_content()
-'''
+    
     e = page.getroot().\
-        find_class('search__alphabet').\
-        pop()
-  '''  
+        find_class('search__filter')
+    
+    links = []
+       
+    for i in e:
+        #links = links + i.attrib.get("href")
+        links.append(i.attrib.get("href"))
+   
     mapping = {
-            'page': page
+            'page': links
         }
 
     body = render("view", mapping)
@@ -55,3 +55,33 @@ broken_html = "https://www.rlsnet.ru/tn_alf_letter_c0.htm"
     #page = etree.parse(StringIO(myString))
 #"<html><head><title>test<body><h1>page title</h3>"
 '''
+'''
+    e = page.getroot().\
+        find_class('search__alphabet').\
+        pop()
+
+
+
+    url_list = ''
+
+    for i in e:
+        url_list = url_list + i
+    print(url_list)
+
+    #page = page.getroot()
+    #page = page.text_content()
+    
+    #t = e.getchildren().pop()
+    #t = t.text_content()
+    
+    alphabet = ''
+       
+    for i in e:
+        alphabet =  alphabet + i.text_content()
+   
+    mapping = {
+            'page': alphabet
+        }
+
+    #"html/aironepage/HTML/index.html"
+  '''

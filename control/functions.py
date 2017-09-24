e602a956e8fe91a3a3b1500b5a10941ad0891cd6
Your Name <swatogor@yandex.ru>
2017-09-24 17:11:07 +0300

    has added new features to parse function. It now outputs a list of dictionaries consists of alphabet letters and links to the pages with medicaments that is started from this letter.

--- control/functions.py
+++ control/functions.py
@@ -24,7 +24,6 @@ db = connection.cursor()
 
 def index():
     mapping = {}
-    
     body = render("index", mapping)
     return body
 
@@ -34,56 +33,44 @@ def get_html(url):
 
 def parse(html):
     soup = BeautifulSoup(html, "html.parser")
-    # 'search_filter' 'j-body body--decreased' 'alphabet_current'
     div = soup.find('div', class_='alphabet__current')    
     
-    for d in div.find_all('a'):
-        print(d)    
+    links = []
+   
+    # Create a dictionary of links and alphabet letters.
+    for i in div.find_all('a')[0:29]:
+        d = {'key': i.text, 'value': i['href']}
+        links.append(d) 
+
+    return links
     
-    #for row in table.find_all('tr')[1:]:
-     #   cols = row.find_all('td')
-      #  print (cols)
- 
 #def get_active_substance():
 
 #def is_homeopathy():
 
 def get_clinical_research(medicament):
-    the_html = parse(get_html('https://www.rlsnet.ru/tn_alf_letter_c0.htm'))
-    #print(the_html)
-    #parse(get_html('https://www.rlsnet.ru/tn_alf_letter_c0.htm'))
-
-    page = html.parse(urlopen('https://www.rlsnet.ru/tn_alf_letter_c0.htm'))
-
-    e = page.getroot().\
-        find_class('search__filter')
-   
-    links = []
-
-    # Create a dictionary of links and alphabet letters.
-    for i in e:   
-        d = {'key': i.text_content(), 'value': i.attrib.get("href")}
-        links.append(d)
+    alphabet_list = parse(get_html('https://www.rlsnet.ru/tn_alf_letter_c0.htm'))
     
     link_togo = ''
-    
+
     # Find a link for transition to the page with particular letter.
-    for i in links:
+    for i in alphabet_list:
         if medicament[0].upper() == i['key']:
-            link_togo = i['value']     
+            link_togo = i['value']
+     
     link_togo = "https:" + link_togo
     
-    page = html.parse(urlopen(link_togo))  
-    root = page.getroot()
-    tree = etree.ElementTree(root)
+ #   page = html.parse(urlopen(link_togo))  
+  #  root = page.getroot()
+   # tree = etree.ElementTree(root)
 
-    children = []
-    for e in tree.iter():
-        d = {'key': e.text, 'value': e.attrib.get("href")} 
-        children.append(d)
+    #children = []
+    #for e in tree.iter():
+     #   d = {'key': e.text, 'value': e.attrib.get("href")} 
+      #  children.append(d)
     
     mapping = {
-            'page': the_html #children
+            'page': link_togo
         }
    
     body = render("view", mapping)
